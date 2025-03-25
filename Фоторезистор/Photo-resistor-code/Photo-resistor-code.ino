3a6fc009853acdf5156366ece1843cc95f6daf5a
Zerocummer <apivovara@gmail.com>
2025-03-25 14:19:40 +0300

    m

--- Фоторезистор/Photo-resistor-code/Photo-resistor-code.ino
+++ Фоторезистор/Photo-resistor-code/Photo-resistor-code.ino
@@ -1,19 +1,23 @@
 void setup() {
   Serial.begin(9600);
-  pinMode(A0, INPUT)
+  pinMode(A0, INPUT);
 }
 
 void loop() {
-  int val = analogRead(A0)
+  int val = analogRead(A0);
   if (val < 340){
-    Serial.println(map(val, 0, 340, 1350, 300));
+    int d1 = map(val, 0, 340, 1350, 300);
+    Serial.println(d1);
   }
   else{
     if (val <= 500){
-      Serial.println(map(val, 340, 500, 300, 110));
+      int d2 = map(val, 340, 500, 300, 110);
+      Serial.println(d2);
     }
     else{
-      Serial.println(map(500, 1023, 110, 0));
+      int d3 = map(val, 500, 1023, 110, 0);
+      Serial.println(d3);
+  }
   }
   delay(50);
 }
