61bbef88d39122e254f7fae97e9b1670d4c11597
SOJuse <apivovara@gmail.com>
2025-12-30 12:57:03 +0300

    d

--- Мышка для инвалидов/sketch_joystick/sketch_joystick.ino
+++ Мышка для инвалидов/sketch_joystick/sketch_joystick.ino
@@ -3,10 +3,11 @@
  #include <Mouse.h>
  #include <Keyboard.h>
  MPU6050 mpu(Wire);
- unsigned long timer = 0;
- #define ENC_A 3
- #define ENC_B 4
+ #define ENC_A 4
+ #define ENC_B 5
  bool p0, p1;
+ int wheel = 0;
+ unsigned long timer = 0;
  int Xang, Yang, Zang;
  int linkerknop = 7;
  int middenknop = 8;
@@ -15,7 +16,6 @@
   pinMode(linkerknop, INPUT_PULLUP);
   pinMode(middenknop, INPUT_PULLUP);
   pinMode(rechterknop, INPUT_PULLUP);
-  Serial.begin(115200);
   Wire.begin();
   byte status = mpu.begin();
   while (status != 0) { } 
@@ -25,13 +25,12 @@
   Keyboard.begin(); 
  }
  void loop() {
- mpu.update();
+ mpu.update();  
  if ((millis() - timer) > 60) { 
-  //чтение данных с гироскопа
+  //проверяем наклон гироскопа
   Xang=mpu.getAngleX();
   Yang=mpu.getAngleY();
   Zang=mpu.getAngleZ();
-  
   int liknopstate = digitalRead(linkerknop);
   int miknopstate = digitalRead(middenknop);
   int reknopstate = digitalRead(rechterknop);
@@ -58,7 +57,8 @@
   else {
     Mouse.press(MOUSE_MIDDLE);
     vlagmi = 1;
-    }
+    } 
+   
    if ( reknopstate > 0){
    if (vlagre = 1){
     Mouse.release(MOUSE_RIGHT);
@@ -67,35 +67,45 @@
     }
   else {
     Mouse.press(MOUSE_RIGHT);
-    vlagre = 1;
+    vlagre=1;
     }   
+    
+    
    if ((Yang > 6) || (Yang < -6) || (Xang > 6) || (Xang < -6)){  
+  
      if (Yang > 6){
      Yang = map(Yang, 6, 10, 1, 10);
      }
+
      else if (Yang < -6) {
      Yang = map(Yang, -10, -6, -10, -1); 
      }
+      
      else if (Xang > 6) {  
      Xang = map(Xang, 6, 10, 1, 10); 
      }
+
      else if (Xang < -6) {
      Xang = map(Xang, -10, -6, -10, -1); 
      }
      Mouse.move(Yang, Xang, 0);
-     pollEnc(digitalRead(ENC_A), digitalRead(ENC_B));
    }
-  timer = millis();
+   pollEnc(digitalRead(ENC_A), digitalRead(ENC_B));
+   if (wheel != 0){
+    Mouse.move(0, 0, wheel);
+   }
+   wheel = 0;
+   timer = millis();
    }}
-void pollEnc(bool e0, bool e1) {
-    if (p0 ^ p1 ^ e0 ^ e1) {
-        if (p1 ^ e0){
-        Mouse.move(0, 0, 6);
-        }
-        else{
-          Mouse.move(0, 0, -6);
-        }    // направление
-        p0 = e0;
-        p1 = e1;
+  void pollEnc(bool e0, bool e1) {
+  if (p0 ^ p1 ^ e0 ^ e1) {
+      if (p1 ^ e0){
+        wheel = 1;
+      }
+      else{
+        wheel = -1;
+      }
+      p0 = e0;
+      p1 = e1;
     }
 }
